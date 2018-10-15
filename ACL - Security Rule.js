dc25375744f51d6ec109a609c57cc90513ffe6bb
parwood <patrick.arwood@gmail.com>
2018-10-15 10:55:37 -0700

    acl change

--- ACL - Security Rule.js
+++ ACL - Security Rule.js
@@ -13,3 +13,43 @@ else if (current.isValidRecord() && owner == true)
 //Otherwise it's false
 else
     answer = false;
+
+    /* global GlideAggregate, GlideRecord, gs */
+    var parent = new GlideRecord('u_product_support_case');
+    parent.get('ad6ce161db560740d9ca72ec0f9619f5');
+    var current = new GlideRecord('task');
+
+    (function refineQuery (current, parent) {
+      var queries = [];
+      var ids = [];
+      var query = '';
+      // look at incidents where this case is associated;
+      var incident = new GlideAggregate('incident');
+      incident.addQuery('u_product_support_case', parent.getValue('sys_id'));
+      incident.addQuery('problem_idISNOTEMPTY');
+      incident.groupBy('problem_id');
+      incident.query();
+      if (incident.hasNext()) {
+        while (incident.next()) {
+          // for each incident associated get all problems and problem tasks
+          ids.push(incident.getValue('problem_id'));
+          query = 'sys_id=' + incident.getValue('problem_id');
+          query += '^ORref_problem_task.problem=' + incident.getValue('problem_id');
+          queries.push(query);
+        }
+      } else {
+        queries = ['sys_id=-1'];// if no incs, set array to query that results in 0 results
+      }
+      // gs.print(ids);
+      // current.addQuery('sys_idIN' + ids);
+      // gs.print(queries.join('^NQ'));
+      var task = new GlideAggregate('task');
+      task.addQuery(queries.join('^OR'));
+      task.groupBy('sys_id');
+      task.query();
+      while (task.next()) {
+        ids.push(task.getValue('sys_id'));
+      }
+      gs.print(ids);
+      current.addQuery('sys_idIN' + ids);
+    })(current, parent);
